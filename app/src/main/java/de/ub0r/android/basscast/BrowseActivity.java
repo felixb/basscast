1eb27fd484deb28c3b5584227d6e47e3653208f3
Felix Bechstein <f@ub0r.de>
2016-01-01 10:26:12 +0100

    minor ui improvements

--- app/src/main/java/de/ub0r/android/basscast/BrowseActivity.java
+++ app/src/main/java/de/ub0r/android/basscast/BrowseActivity.java
@@ -276,21 +276,6 @@ public class BrowseActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public boolean dispatchKeyEvent(final KeyEvent event) {
         final int action = event.getAction();
         final int keyCode = event.getKeyCode();
