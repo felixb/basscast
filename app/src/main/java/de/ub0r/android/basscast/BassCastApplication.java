27bd2aaadd7c8482620c46fac2f5dfa02f3554c0
Felix Bechstein <f@ub0r.de>
2016-01-24 09:29:03 +0100

    insert default streams only once, refs #30

--- app/src/main/java/de/ub0r/android/basscast/BassCastApplication.java
+++ app/src/main/java/de/ub0r/android/basscast/BassCastApplication.java
@@ -1,48 +1,29 @@
 package de.ub0r.android.basscast;
 
 import android.app.Application;
-import android.content.ContentResolver;
-
-import de.ub0r.android.basscast.model.MimeType;
-import de.ub0r.android.basscast.model.Stream;
-import de.ub0r.android.basscast.model.StreamsTable;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 
 /**
  * @author flx
  */
 public class BassCastApplication extends Application {
 
+    private final static String PREFS_DEFAULT_STREAMS_INSERTED = "default_streams_inserted";
+
     @Override
     public void onCreate() {
         super.onCreate();
-        initDummyStreams();
+        insertDefaultStreams();
     }
 
-    private void initDummyStreams() {
-        ContentResolver cr = getContentResolver();
-        if (cr.query(StreamsTable.CONTENT_URI, null, null, null, null).getCount() == 0) {
-            cr.insert(StreamsTable.CONTENT_URI, StreamsTable.getContentValues(new Stream(
-                    "http://www.tagesschau.de/sendung/tagesschau/index.html",
-                    "Tagesschau",
-                    new MimeType("text/html")
-            ), false));
-
-            cr.insert(StreamsTable.CONTENT_URI, StreamsTable.getContentValues(new Stream(
-                    "http://radioeins.de/stream",
-                    "radio 1",
-                    new MimeType("audio/mp3")
-            ), false));
-
-            cr.insert(StreamsTable.CONTENT_URI, StreamsTable.getContentValues(new Stream(
-                    "http://amsterdam2.shouthost.com.streams.bassdrive.com:8000/;",
-                    "bassdrive",
-                    new MimeType("audio/mpeg")
-            ), false));
-            cr.insert(StreamsTable.CONTENT_URI, StreamsTable.getContentValues(new Stream(
-                    "http://archives.bassdrivearchive.com",
-                    "bassdrive archives",
-                    new MimeType("text/html")
-            ), false));
+    private void insertDefaultStreams() {
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        if (!prefs.getBoolean(PREFS_DEFAULT_STREAMS_INSERTED, false)) {
+            StreamUtils.insertDefaultStreams(this);
+            prefs.edit()
+                    .putBoolean(PREFS_DEFAULT_STREAMS_INSERTED, true)
+                    .apply();
         }
     }
 }
--- app/src/main/java/de/ub0r/android/basscast/StreamUtils.java
+++ app/src/main/java/de/ub0r/android/basscast/StreamUtils.java
@@ -1,7 +1,9 @@
 package de.ub0r.android.basscast;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 
 import de.ub0r.android.basscast.model.Stream;
 import de.ub0r.android.basscast.model.StreamsTable;
@@ -26,5 +28,24 @@ public class StreamUtils {
                 StreamsTable.FIELD__ID + "=? or "
                         + StreamsTable.FIELD_BREADCRUMBS + " like '%'||?||'%'",
                 new String[]{id, id});
+    }
+
+    public static void insertDefaultStreams(final Context context) {
+        insertStreams(context.getContentResolver(), getDefaultStreams(context));
+    }
+
+    private static Stream[] getDefaultStreams(final Context context) {
+        String[] uris = context.getResources().getStringArray(R.array.default_streams);
+        Stream[] streams = new Stream[uris.length];
+        for (int i = 0; i < uris.length; i++) {
+            streams[i] = new Stream(Uri.parse(uris[i]));
+        }
+        return streams;
+    }
+
+    private static void insertStreams(final ContentResolver cr, final Stream[] streams) {
+        for (Stream stream : streams) {
+            cr.insert(StreamsTable.CONTENT_URI, StreamsTable.getContentValues(stream, false));
+        }
     }
 }
